d67e2d345cb3b7a27c9a9506b30ca0d09ba97b79
ncdemo18 <ncdemo18@gmail.com>
2018-11-22 13:18:35 +0400

    add folders

--- src/main/java/com/netcracker/jgit/Main.java
+++ src/main/java/com/netcracker/jgit/Main.java
@@ -3,48 +3,69 @@ package com.netcracker.jgit;
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
-import org.eclipse.jgit.transport.CredentialsProvider;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
-    private static final String REMOTE_URL = "https://github.com/user/testJGitRepository.git";
+    private static final String REMOTE_URL = "https://github.com/ncdemo18/testJGitRepository.git";
+    private static final String USERNAME = "ncdemo18";
+    private static final String PASSWORD = "****";
 
     public static void main(String[] args) throws GitAPIException, IOException {
-        File localPath = File.createTempFile("TestGitRepository", "");
+        System.out.print("Enter location name: ");
+        Scanner scanner = new Scanner(System.in);
+        String locationName = scanner.nextLine();
+
+        Path sourcePath = Paths.get("mockup//london//bg");
+        List<Path> collect = Arrays
+                .stream(Objects.requireNonNull(sourcePath.toFile().listFiles()))
+                .filter((file -> !file.isDirectory()))
+                .map(file -> Paths.get(file.getAbsolutePath()))
+                .collect(Collectors.toList());
+
+        for (Path path : collect) {
+            System.out.println(path.toString());
+        }
+
+        String strTargetPath = "mockup//" + locationName + "//bg";
+        Path directory = Files.createDirectories(Paths.get(strTargetPath));
+        // Files.createFile(Paths.get(strTargetPath + "//" + "file.txt"));
+        Files.copy(sourcePath.resolve("file.txt"), Paths.get(strTargetPath + "//" + "file.txt"));
+        // addOnRepository();
+    }
+
+    public static void addLocationOnRepository(String locationName, List<Path> files) throws GitAPIException, IOException{
+        File localPath = File.createTempFile("testJGitRepository", "");
         if (!localPath.delete()) {
             throw new IOException("Could not delete temporary file " + localPath);
         }
 
-        // clone repository
         System.out.println("Cloning from " + REMOTE_URL + " to " + localPath);
         try (Git result = Git.cloneRepository()
                 .setURI(REMOTE_URL)
                 .setDirectory(localPath)
                 .call()) {
-           System.out.println("Having repository: " + result.getRepository().getDirectory());
+            System.out.println("Having repository: " + result.getRepository().getDirectory());
 
             File myFile = new File(result.getRepository().getDirectory().getParent(), "testfile");
-            if(!myFile.createNewFile()) {
+            if (!myFile.createNewFile()) {
                 throw new IOException("Could not create file " + myFile);
             }
 
-            // run the add
             result.add().addFilepattern("testfile").call();
-
-            // and then commit the changes
             result.commit().setMessage("Added testfile").call();
             System.out.println("Committed file " + myFile + " to repository at " + result.getRepository().getDirectory());
 
-            result.push().setCredentialsProvider(new UsernamePasswordCredentialsProvider("user", "password")).call();
+            result.push().setCredentialsProvider(new UsernamePasswordCredentialsProvider(USERNAME, PASSWORD)).call();
             System.out.println("Pushed from repository: " + result.getRepository().getDirectory() + " to remote repository at " + REMOTE_URL);
-
         }
-
         FileUtils.deleteDirectory(localPath);
     }
 }
